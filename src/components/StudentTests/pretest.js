03ee7ea35ba2b1065cb9ef08822551d4ebaaf23b
Brandon Chen <brandonchenjiacheng@gmail.com>
2020-05-09 02:53:55 -0700

    moved instructions to repl readme

--- src/components/StudentTests/pretest.js
+++ src/components/StudentTests/pretest.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import rough from "roughjs/bin/rough";
 import CodeFormat from "../Utilities/CodeFormatter.js";
-import Iframe from "react-iframe";
 
 class PreTest extends Component {
   test = React.createRef();
   canvasOne = React.createRef();
-  canvasTwo = React.createRef();
 
   componentDidMount() {
     var box = this.test.current;
@@ -16,17 +14,6 @@ class PreTest extends Component {
 
     var rc = rough.canvas(canvasOne);
     rc.rectangle(10, 10, canvasOne.width - 80, canvasOne.height - 100); // x, y, width, height
-
-    var canvasTwo = this.canvasTwo.current;
-    canvasTwo.width = box.offsetWidth + 100;
-    canvasTwo.height = box.offsetHeight * 3 + 100;
-
-    var rc2 = rough.canvas(canvasTwo);
-    rc2.rectangle(10, 10, canvasTwo.width - 80, canvasTwo.height - 100, {
-      bowing: 2,
-      stroke: "white",
-      strokeWidth: 2,
-    });
   }
 
   render() {
@@ -42,51 +29,28 @@ class PreTest extends Component {
               This will help us get a sense for your current coding skills.
             </h2>
             <p>
-              <strong>GOAL:</strong> Figure out how to tailor content to you so
+              <strong>GOAL: </strong>Figure out how to tailor content to you so
               we minimize the amount of time we spend repeating content you
               already know. It will not count against you in any way.
             </p>
             <p>
-              Below, you'll see a mini-IDE that's been prepared for you to code
-              in. To get started, hit the "Files" icon on the upper left and
-              switch to "main.cs".
+              See this{" "}
+              <a
+                href="https://github.com/godith0/eureka-moment-repls/blob/pretest/README.md"
+                target="_blank"
+              >
+                README
+              </a>{" "}
+              for information on how to get started.
             </p>
           </div>
-        </section>
-
-        <section className="hero is-info">
-          <div className="hero-body">
-            <div className="container">
-              <canvas ref={this.canvasTwo}></canvas>
-              <h1 className="title">Problem #X</h1>
-              <h2 className="subtitle">Unique Names</h2>
-
-              <p>
-                Implement the UniqueNames method. When passed two arrays of
-                names, it will return an array containing the names that appear
-                in either or both arrays. The returned array should have no
-                duplicates.
-              </p>
-              <p>
-                For example, calling the following should return an array
-                containing <strong>Ava, Emma, Olivia, and Sophia</strong> in any
-                order:
-              </p>
-              <CodeFormat>{`
-                MergeNames.UniqueNames(new string[]{'Ava', 'Emma', 'Olivia'}, new string[]{'Olivia', 'Sophia', 'Emma'})
-             `}</CodeFormat>
-            </div>
-          </div>
-          <Iframe
-            height="1000px"
+          <hr></hr>
+          <iframe
+            frameborder="0"
             width="100%"
-            src="https://repl.it/@brandonchen95/unique-names?lite=true"
-            scrolling="no"
-            frameborder="no"
-            allowtransparency="true"
-            allowfullscreen="true"
-            sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"
-          />
+            height="800px"
+            src="https://repl.it/@brandonchen95/eureka-moment-repls?lite=true"
+          ></iframe>
         </section>
       </div>
     );
