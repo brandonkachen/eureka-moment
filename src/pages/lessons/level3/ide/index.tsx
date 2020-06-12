31807e3d2f1cc48722f6eaab23ee7b57615a4c74
Brandon Chen <brandonchenjiacheng@gmail.com>
2020-06-11 17:37:37 -0700

    moved orig level2 to level3

---
+++ src/pages/lessons/level3/ide/index.tsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react"
+
+import Layout from "components/layout"
+import SEO from "components/seo"
+import IDEStyle from "components/mdx-styles/ide"
+
+import { Link, navigate } from "gatsby"
+
+import LessonMD from "./sidebar.mdx"
+import Protected from "components/protected"
+
+class Level3 extends Component {
+  render() {
+    return (
+      <Layout styleOverride={{ maxWidth: 1400 }} mdxStyle={IDEStyle}>
+        <Protected>
+          <SEO title="Level 3" />
+          {this.lesson()}
+        </Protected>
+      </Layout>
+    )
+  }
+
+  lesson() {
+    return (
+      <div className="box">
+        <div className="columns">
+          <div className="column is-two-fifths">
+            <div className="overflow">
+              <LessonMD></LessonMD>
+              <div className="has-text-centered">
+                <button
+                  className="button is-success is-light"
+                  onClick={() =>
+                    navigate("/lessons/level3", {
+                      state: { completed: true },
+                    })
+                  }
+                >
+                  Finish
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <iframe
+              title="repl level3"
+              width="100%"
+              height="100%"
+              src="https://repl.it/@brandonchen95/eureka-moment-level3?lite=true"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Level2
---
+++ src/pages/lessons/level3/index.tsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react"
+
+import Layout from "components/layout"
+import SEO from "components/seo"
+import { Link } from "gatsby"
+import { navigate } from "gatsby"
+
+import IntroMD from "./intro.mdx"
+import OutroMD from "./outro.mdx"
+import Protected from "components/protected"
+
+interface IProps {
+  location: {
+    state: {
+      completed?: boolean
+    }
+  }
+}
+
+interface IState {}
+
+class Level3 extends Component<IProps, IState> {
+  render() {
+    var { location } = this.props
+
+    const mdxToShow =
+      location.state && location.state.completed ? (
+        <OutroMD></OutroMD>
+      ) : (
+        <IntroMD></IntroMD>
+      )
+    return (
+      <Layout>
+        <Protected>
+          <SEO title="Level 3" />
+          {mdxToShow}
+          <div className="has-text-centered">
+            <button
+              className="button is-info is-light"
+              onClick={() => navigate("/lessons/level3/ide")}
+            >
+              Start Lesson
+            </button>
+          </div>
+        </Protected>
+      </Layout>
+    )
+  }
+}
+
+export default Level3
